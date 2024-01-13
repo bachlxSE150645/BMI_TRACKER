20cbef4ba0642afe79e6059637012fefeaab168f
bách lương <xuanbach2512.2000@gmail.com>
2024-01-13 15:48:02 +0700

    sua lai mapper

--- BussinessObject/MapData/loginData.cs
+++ BussinessObject/MapData/loginData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,12 @@ namespace BussinessObject.MapData
 {
     public class loginData
     {
-        public string Email { get; set; }
-        public string passWord { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [Required]
+        public string email { get; set; }
+        [DataType(DataType.Password)]
+        [Required]
+        public string password { get; set; }
+       
     }
 }
--- BussinessObject/MapData/signUpData.cs
+++ BussinessObject/MapData/signUpData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,15 @@ namespace BussinessObject.MapData
 {
     public class signUpData
     {
-        public string fullname { get; set; }
-        public string Password { get; set; }
-        public string Email { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [Required]
+        public string email { get; set; }
+        public string fullName { get; set; }
+        [DataType(DataType.Password)]
+        [Required]
+        public string password { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
         public string phoneNumber { get; set; }
 
     }
